d71bfc2d4dfd9f5dd73c37050937e8b6c8da4038
ykxie <ykxie@kaikeba.com>
2017-09-23 16:15:48 +0800

    add package and style

--- src/App.vue
+++ src/App.vue
@@ -11,9 +11,6 @@ export default {
 }
 </script>
 
-
-
-
 <style>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
+import 'babel-polyfill'
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import fastclick from 'fastclick'
+
+import 'common/stylus/index.styl'
+// 移动端300毫秒延时
+fastclick.attach(document.body)
 
 Vue.config.productionTip = false
 
