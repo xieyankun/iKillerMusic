e72826544ec605c440253bd88df058e12941e83d
ykxie <ykxie@kaikeba.com>
2017-09-23 22:41:11 +0800

    add recommend

--- src/common/js/jsonp.js
+++ src/common/js/jsonp.js
@@ -1,7 +1,8 @@
 import originJSONP from 'jsonp'
 
 export default function jsonp(url, data, option) {
-  url = (url.indexOf() < 0 ? '?' : '&') + param(data)
+  url += (url.indexOf('?') < 0 ? '?' : '&') + param(data)
+
   return new Promise((resolve, reject) => {
     originJSONP(url, option, (err, data) => {
       if (!err) {
@@ -16,8 +17,8 @@ export default function jsonp(url, data, option) {
 function param(data) {
   let url = ''
   for (var k in data) {
-    let value = data[k] !== undefind ? data[0] : ''
+    let value = data[k] !== undefined ? data[0] : ''
     url += `&${k}=${encodeURIComponent(value)}`
   }
-  return url ? url.subString(1) : ''
+  return url ? url.substring(1) : ''
 }
---
+++ src/components/recommend/recommend.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="recommend" ref="recommend">
+    
+  </div>
+</template>
+
+<script>
+import {getRecommend} from 'api/recommend'
+export default {
+  created() {
+    this._getRecommend()
+  },
+  methods: {
+    _getRecommend() {
+      getRecommend().then((data) => {
+        console.log(data)
+      })
+    }
+  }
+}
+</script>
